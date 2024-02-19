c266201646c62c7a06150961b37332e4aa5b89a9
theinkjetprinter <theinkjetprinter@gmail.com>
2024-02-19 14:23:41 -0700

    Quicksave to leave
    work on more

---
+++ main.py
@@ -0,0 +1,50 @@
+# import a utility function for loading Roboflow models
+import os
+from inference import get_roboflow_model
+# import supervision to visualize our results
+import supervision as sv
+# import cv2 to helo load our image
+import cv2
+import time
+
+start = time.time()
+# define the image url to use for inference
+video = cv2.VideoCapture(2)
+
+# load a pre-trained yolov8n model
+model = get_roboflow_model(model_id="note-detection-frc-2024/4", api_key="Q9t3AxF6Ra8qoPV2RqeC")
+i = 0
+while True:
+    i+=1
+    succses, img = video.read()
+    #imgJpg = cv2.imwrite("frame%d.jpg" % i, img)
+    jpgImage = cv2.imencode('.jpg', img)[i].tostring()
+    results = model.infer(jpgImage)
+     
+    # take images, put them into a folder, then delete them when done
+
+    detections = sv.Detections.from_inference(results[0].dict(by_alias=True, exclude_none=True))
+
+    # # create supervision annotators
+    # bounding_box_annotator = sv.BoundingBoxAnnotator()
+    # label_annotator = sv.LabelAnnotator()
+
+
+
+    # # annotate the image with our inference results
+    # annotated_image = bounding_box_annotator.annotate(
+    #     scene=imgJpg, detections=detections)
+    # annotated_image = label_annotator.annotate(
+    #     scene=annotated_image, detections=detections)
+    print(detections['class_name'])
+    try:
+        for item in detections['class_name']:
+            if item == 'note':
+                print("True")
+            
+    except:
+        pass
+
+    os.remove(f"frame{i}.jpg")
+
+
