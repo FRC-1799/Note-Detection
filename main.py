16d48f4990bb65c1eadbfe30fa616cd7e7d6d0ae
oof15642 <59772241+oof15642@users.noreply.github.com>
2024-02-29 20:38:07 -0700

    removed unecessary

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# import a utility function for loading Roboflow models
-import os
 from inference import get_roboflow_model
-# import supervision to visualize our results
 import supervision as sv
-# import cv2 to helo load our image
 import cv2
 import time
 
@@ -20,8 +16,6 @@ while True:
     #imgJpg = cv2.imwrite("frame%d.jpg" % i, img)
     jpgImage = cv2.imencode('.jpg', img)[1].tobytes()
     results = model.infer(jpgImage)
-     
-    # take images, put them into a folder, then delete them when done
 
     detections = sv.Detections.from_inference(results[0].dict(by_alias=True, exclude_none=True))
 
@@ -29,15 +23,11 @@ while True:
     # bounding_box_annotator = sv.BoundingBoxAnnotator()
     # label_annotator = sv.LabelAnnotator()
 
-
-
     # # annotate the image with our inference results
     # annotated_image = bounding_box_annotator.annotate(
     #     scene=imgJpg, detections=detections)
     # annotated_image = label_annotator.annotate(
     #     scene=annotated_image, detections=detections)
-
-
 
     try:
         for item in detections['class_name']:
@@ -49,7 +39,5 @@ while True:
                 print(detections.xyxy[0][2] - detections.xyxy[0][0])
     except:
         pass
-
-    #os.remove(f"frame{i}.jpg")
 
 
