70f0158c6617e339d4b9341360e811f9dc6dd2e6
Fooot-Code <brennon.hill@gmail.com>
2025-02-18 18:27:35 -0700

    refactored and cool

--- main.py
+++ main.py
@@ -48,71 +48,85 @@ def main():
     # Reef Values
     reef = [[False for _ in range(4)] for _ in range(12)]
     defaultReef = [False for _ in range(4)]
+    defaultAlgae = [False for _ in range(2)]
 
     # Grabs each of the topics for Network Tables
     robotPoseTopic = inst.getStructTopic("RobotPose", Pose3d)
     robotPosePublisher = robotPoseTopic.getEntry("Pose3d")
+    aprilTagCameraConnectionTopic = inst.getBooleanTopic("AprilTagCameraConnection")
+    aprilTagCameraConnectionPublisher = aprilTagCameraConnectionTopic.getEntry("CameraConnected").publish()
 
+    # Reef Publishers and Subscribers
     reefTable = inst.getTable("CoralLocations")
     reefL1Topic = reefTable.getBooleanArrayTopic("ReefL1")
     reefL2Topic = reefTable.getBooleanArrayTopic("ReefL2")
     reefL3Topic = reefTable.getBooleanArrayTopic("ReefL3")
     reefL4Topic = reefTable.getBooleanArrayTopic("ReefL4")
-    reefSubscribers = [reefL1Topic.subscribe(defaultReef), reefL2Topic.subscribe(defaultReef), reefL3Topic.subscribe(defaultReef), reefL4Topic.subscribe(defaultReef)]
-    reefPublishers = [reefL1Topic.publish(), reefL2Topic.publish(), reefL3Topic.publish(), reefL4Topic.publish()]
+    algae1Topic = reefTable.getBooleanArrayTopic("Algae1")
+    algae2Topic = reefTable.getBooleanArrayTopic("Algae2")
+    coralSubscribers = [reefL1Topic.subscribe(defaultReef), reefL2Topic.subscribe(defaultReef), reefL3Topic.subscribe(defaultReef), reefL4Topic.subscribe(defaultReef)] 
+    coralPublishers = [reefL1Topic.publish(), reefL2Topic.publish(), reefL3Topic.publish(), reefL4Topic.publish()]
+    algaeSubscribers = [algae1Topic.publish(defaultAlgae), algae2Topic.publish(defaultAlgae)]
+    algaePublishers = [algae1Topic.publish(), algae2Topic.publish()]
+    reefCameraConnectionTopic = inst.getBooleanTopic("ReefCameraConnection")
 
-    fuckyou = inst.getTable("reefPose3dTable")
-    mommy = fuckyou.getStructArrayTopic("pose", Pose3d)
-    ahhhPublisher = mommy.publish()
 
-    team = "blue"
+    # Reef Pose3D for debugging purposes
+    reefPose3dTable = inst.getTable("reefPose3dTable")
+    pose3dTableTopic = reefPose3dTable.getStructArrayTopic("pose", Pose3d)
+    pose3dPublisher = pose3dTableTopic.publish()
 
     # Create an instance of the AprilTag camera
-    #grabAprilTagInformation = AprilTagCamera(PhotonLibConstants.APRIL_TAG_CAMERA_NAME)
-    coralCamera = CoralCamera.CoralCamera()
+    aprilTagCamera = AprilTagCamera(PhotonLibConstants.APRIL_TAG_CAMERA_NAME)
+    aprilTagCameraOpened = aprilTagCamera.isConnected()
+    aprilTagCameraConnectionPublisher.set(True if aprilTagCameraOpened else False)
 
+    coralCamera = CoralCamera.CoralCamera()
+    coralCameraOpened = coralCamera.camera.isOpened()
+    reefCameraConnectionTopic.set(True if coralCameraOpened else False)
+    
     hitboxMakerClass = CreateHitbox()
     hitboxes = hitboxMakerClass.coralHitboxMaker()
     hitboxAlgae = hitboxMakerClass.algaeHitboxMaker()
 
-#java -jar PhotonVisionJar/photonvision-v2024.3.1-linuxx64.jar
     running = True
     while running:
         # Get April Tags from the camera
-        #aprilTags = grabAprilTagInformation.get_tags()
-
         if keyboard.is_pressed("q"):
             inst.stopServer()
             cv2.destroyAllWindows()
             break
-
-        # if aprilTags:
-        #     robot_position_process = multiprocessing.Process(target=fetch_robot_position)
+        
+        if aprilTagCamera.isConnected():
+            aprilTags = aprilTagCamera.get_tags()
+            if aprilTags:
+                robot_position_process = multiprocessing.Process(target=fetch_robot_position)
+                robot_position_process.start()
+                robot_position_process.join()
+                position, timestamp = fetch_robot_position()
             
-        #     robot_position_process.start()
-        #     robot_position_process.join()
-        #     position, timestamp = fetch_robot_position()
-        
-        #     if position:
-        #         robotPosePublisher.set(position.estimatedPose, timestamp)
+                if position:
+                    robotPosePublisher.set(position.estimatedPose, timestamp)
 
-        reef = grab_past_reef(reefSubscribers)
-        coralCamera.camera_loop(reef, "algea", hitboxes, "none")
 
-        branchList = hitboxMakerClass.returnBranchesList()
-        poseList = []
-        for branch in branchList:
-            poseList.extend([branch.L1.first_placement_pose, branch.L2.ideal_coral_placement_pose, branch.L3.ideal_coral_placement_pose, branch.L4.ideal_coral_placement_pose])
-            #poseList.extend([branch.L2.first_placement_pose, branch.L3.first_placement_pose])
-        
-        
-        for level, publisher in enumerate(reefPublishers):
-            reefLevelBoolVals = []
-            for reefSection in reef:
-                reefLevelBoolVals.append(reefSection[level])
-            publisher.set(reefLevelBoolVals)
+        if coralCameraOpened:
+            reef = grab_past_reef(coralSubscribers)
+            coralCamera.camera_loop(reef, "algea", hitboxes, "none",5)
 
-        ahhhPublisher.set(poseList)
+            branchList = hitboxMakerClass.returnBranchesList()
+            poseList = []
+            for branch in branchList:
+                poseList.extend([branch.L1.first_placement_pose, branch.L2.ideal_coral_placement_pose, branch.L3.ideal_coral_placement_pose, branch.L4.ideal_coral_placement_pose])
+                #poseList.extend([branch.L2.first_placement_pose, branch.L3.first_placement_pose])
+            
+            
+            for level, publisher in enumerate(coralPublishers):
+                reefLevelBoolVals = []
+                for reefSection in reef:
+                    reefLevelBoolVals.append(reefSection[level])
+                publisher.set(reefLevelBoolVals)
+
+            pose3dPublisher.set(poseList)
             
 
         
