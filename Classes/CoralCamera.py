e87d02dd11923f82363db6d987e270b497c3eea3
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2025-02-25 15:38:34 -0700

    Comp Changes

--- Classes/CoralCamera.py
+++ Classes/CoralCamera.py
@@ -1,4 +1,4 @@
-from ConstantsAndUtils.Constants import CameraConstants
+from ConstantsAndUtils.Constants import CoralAndAlgaeCameraConstants
 import cv2
 from ultralytics import YOLO
 import math
@@ -10,18 +10,17 @@ class CoralCamera:
     Detects coral and makes a ray to said coral to see which level it is on. This data gets published to NetworkTables
     """
 
-    def __init__(self, cameraIndex: int = 0, modelPath: str = "Models/runs/detect/train/weights/BestModel.pt"):
+    def __init__(self, cameraIndex, modelPath: str = "Models/runs/detect/train/weights/BestModel.pt"):
         self.self = self
         self.cameraIndex = cameraIndex
         
         self.model = YOLO(modelPath)
-        self.screenWidth = CameraConstants.horizontalPixels
-        self.screenHeight = CameraConstants.verticalPixels
+        self.screenWidth = CoralAndAlgaeCameraConstants.horizontalPixels
+        self.screenHeight = CoralAndAlgaeCameraConstants.verticalPixels
         self.camera = cv2.VideoCapture(cameraIndex)
 
     def camera_loop(self, reef: list[list[bool]], algae: list[list[bool]], reefHitboxes: list, algaeHitboxes: list, algaeNotSeenCounter: list, robotPosition):
         ret, frame = self.camera.read()
-
 
         if ret:
             frame = cv2.resize(frame, (self.screenWidth, self.screenHeight)) 
@@ -32,28 +31,28 @@ class CoralCamera:
                 for box in result.boxes:
                     conf = box.conf[0].item()
                     cls = int(box.cls[0].item())
-                    if conf > CameraConstants.confidenceTolerance:
-                        if self.model.names[cls].lower() == "coral":
+                    if conf > CoralAndAlgaeCameraConstants.confidenceTolerance:
+                        if self.model.names[cls].lower() == "coral" and CoralAndAlgaeCameraConstants.shouldTestCoral:
 
                             # Top left X, top left Y, bottom right X, bottom right Y
                             x1, y1, x2, y2 = map(int, box.xyxy[0])
                             
 
                             centerOfCoral = ((x2 - x1) / 2 + x1, (y2 - y1) / 2 + y1) 
-                            coralYaw = CameraConstants.reefCameraHorizontalAnglePerPixel * centerOfCoral[0]
-                            coralPitch = CameraConstants.reefCameraVerticalAnglePerPixel * centerOfCoral[1]
+                            coralYaw = CoralAndAlgaeCameraConstants.reefCameraHorizontalAnglePerPixel * centerOfCoral[0]
+                            coralPitch = CoralAndAlgaeCameraConstants.reefCameraVerticalAnglePerPixel * centerOfCoral[1]
 
-                            centerYaw = CameraConstants.reefCameraHorizontalAnglePerPixel * (self.screenWidth / 2)
-                            centerPitch = CameraConstants.reefCameraVerticalAnglePerPixel * (self.screenHeight / 2) + math.radians(10)
+                            centerYaw = CoralAndAlgaeCameraConstants.reefCameraHorizontalAnglePerPixel * (self.screenWidth / 2)
+                            centerPitch = CoralAndAlgaeCameraConstants.reefCameraVerticalAnglePerPixel * (self.screenHeight / 2) + math.radians(10)
 
                             # Adjusts the pitch and yaw so that its center (0, 0) is in the middle of the camera lens
                             coralYaw = coralYaw - centerYaw
                             coralPitch = -(coralPitch - centerPitch)
 
-                            vectorOfCoral = Vector.vector(robotPosition.transformBy(CameraConstants.ROBOT_TO_CAMERA_ROTATED_TRANSFORMATION), coralPitch, coralYaw)
+                            vectorOfCoral = Vector.vector(robotPosition.transformBy(CoralAndAlgaeCameraConstants.ROBOT_TO_CAMERA_ROTATED_TRANSFORMATION), coralPitch, coralYaw)
 
                             # Loops again for a certain increment across the line, and the increment acts as the x value for the equation
-                            for increment in range(1, CameraConstants.vectorLengthToExtend):
+                            for increment in range(1, CoralAndAlgaeCameraConstants.vectorLengthToExtend):
                                 positionLocation = vectorOfCoral.getPoseAtStep(increment)
                                 if vectorAlreadyCollided:
                                     break
@@ -78,54 +77,56 @@ class CoralCamera:
                             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                             cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                         
-                        # else: # Is algae
-                        #     x1, y1, x2, y2 = map(int, box.xyxy[0])
+                        elif self.model.names[cls].lower() == "algae" and CoralAndAlgaeCameraConstants.shouldTestAlgae:
+                            x1, y1, x2, y2 = map(int, box.xyxy[0])
                             
 
-                        #     centerOfAlgae = ((x2 - x1) / 2 + x1, (y2 - y1) / 2 + y1) 
-                        #     algaeYaw = CameraConstants.reefCameraHorizontalAnglePerPixel * centerOfAlgae[0]
-                        #     algaePitch = CameraConstants.reefCameraVerticalAnglePerPixel * centerOfAlgae[1]
+                            centerOfAlgae = ((x2 - x1) / 2 + x1, (y2 - y1) / 2 + y1) 
+                            algaeYaw = CoralAndAlgaeCameraConstants.reefCameraHorizontalAnglePerPixel * centerOfAlgae[0]
+                            algaePitch = CoralAndAlgaeCameraConstants.reefCameraVerticalAnglePerPixel * centerOfAlgae[1]
 
-                        #     centerYaw = CameraConstants.reefCameraHorizontalAnglePerPixel * (self.screenWidth / 2)
-                        #     centerPitch = CameraConstants.reefCameraVerticalAnglePerPixel * (self.screenHeight / 2)
+                            centerYaw = CoralAndAlgaeCameraConstants.reefCameraHorizontalAnglePerPixel * (self.screenWidth / 2)
+                            centerPitch = CoralAndAlgaeCameraConstants.reefCameraVerticalAnglePerPixel * (self.screenHeight / 2)
 
-                        #     # Adjusts the pitch and yaw so that its center (0, 0) is in the middle of the camera lens
-                        #     algaeYaw = algaeYaw - centerYaw
-                        #     algaePitch = -(algaePitch - centerPitch)
+                            # Adjusts the pitch and yaw so that its center (0, 0) is in the middle of the camera lens
+                            algaeYaw = algaeYaw - centerYaw
+                            algaePitch = -(algaePitch - centerPitch)
 
-                        #     vectorOfAlgae = Vector.vector(robotPosition.transformBy(CameraConstants.ROBOT_TO_CAMERA_ROTATED_TRANSFORMATION), algaePitch, algaeYaw)
+                            vectorOfAlgae = Vector.vector(robotPosition.transformBy(CoralAndAlgaeCameraConstants.ROBOT_TO_CAMERA_ROTATED_TRANSFORMATION), algaePitch, algaeYaw)
 
-                        #     for increment in range(1, CameraConstants.vectorLengthToExtend):
-                        #         positionLocation = vectorOfAlgae.getPoseAtStep(increment)
-                        #         if vectorAlreadyCollided:
-                        #             break
+                            for increment in range(1, CoralAndAlgaeCameraConstants.vectorLengthToExtend):
+                                positionLocation = vectorOfAlgae.getPoseAtStep(increment)
+                                if vectorAlreadyCollided:
+                                    break
                                 
-                        #         for hitboxSection in algaeHitboxes:
-                        #             for hitbox in hitboxSection:
+                                for hitboxSection in algaeHitboxes:
+                                    for hitbox in hitboxSection:
 
-                        #                 hitboxSectionIndex = algaeHitboxes.index(hitboxSection)
-                        #                 hitboxIndex = hitboxSection.index(hitbox)
-                        #                 algaeSeen = hitbox.colidePose3d(positionLocation)
+                                        hitboxSectionIndex = algaeHitboxes.index(hitboxSection)
+                                        hitboxIndex = hitboxSection.index(hitbox)
+                                        algaeSeen = hitbox.colidePose3d(positionLocation)
 
-                        #                 # If we haven't seen the algae for more than the tolerance frames, it is not seen right now, and it is marked as true, then assume it isn't there anymore
-                        #                 if algaeNotSeenCounter[hitboxSectionIndex][hitboxIndex] > CameraConstants.algaeViewedTolerance and not algaeSeen and algae[hitboxSectionIndex][hitboxIndex]:
-                        #                     algae[hitboxSectionIndex][hitboxSection] = False
+                                        # If we haven't seen the algae for more than the tolerance frames, it is not seen right now, and it is marked as true, then assume it isn't there anymore
+                                        if algaeNotSeenCounter[hitboxSectionIndex][hitboxIndex] > CoralAndAlgaeCameraConstants.algaeViewedTolerance and not algaeSeen and algae[hitboxSectionIndex][hitboxIndex]:
+                                            algae[hitboxSectionIndex][hitboxSection] = False
                                         
-                        #                 # If we do see the algae and it is false, mark it as true
-                        #                 if algaeSeen and not algae[hitboxSectionIndex][hitboxIndex]:
-                        #                     algae[hitboxSectionIndex][hitboxIndex] = True
-                        #                     vectorAlreadyCollided = True
-                        #                     break
+                                        # If we do see the algae and it is false, mark it as true
+                                        if algaeSeen and not algae[hitboxSectionIndex][hitboxIndex]:
+                                            algae[hitboxSectionIndex][hitboxIndex] = True
+                                            vectorAlreadyCollided = True
+                                            break
 
-                        #                 # If we don't see the algae, but it is marked as true, increase the counter
-                        #                 elif not algaeSeen and algae[hitboxSectionIndex][hitboxIndex]:
-                        #                     algaeNotSeenCounter[hitboxSectionIndex][hitboxIndex] += 1
-                        #             if vectorAlreadyCollided:
-                        #                 break
+                                        # If we don't see the algae, but it is marked as true, increase the counter
+                                        elif not algaeSeen and algae[hitboxSectionIndex][hitboxIndex]:
+                                            algaeNotSeenCounter[hitboxSectionIndex][hitboxIndex] += 1
+                                    if vectorAlreadyCollided:
+                                        break
                             
-                        #     # When the loop is exited, reset this variable in order to be able to search again
-                        #     vectorAlreadyCollided = False if vectorAlreadyCollided else True
+                            # When the loop is exited, reset this variable in order to be able to search again
+                            vectorAlreadyCollided = False if vectorAlreadyCollided else True
 
 
             cv2.imshow('heheh', frame)
             cv2.waitKey(1)  # Ensures OpenCV window updates properly
+
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import ntcore
 import cv2
 import wpimath
-from ConstantsAndUtils.Constants import PhotonLibConstants, CameraConstants
+from ConstantsAndUtils.Constants import PhotonLibConstants, CoralAndAlgaeCameraConstants
 from Classes.AprilTagCamera import *
 import multiprocessing
 from wpimath.geometry import Pose3d, Transform3d, Translation2d, Rotation2d, Rotation3d
@@ -13,7 +13,7 @@ from wpimath.units import degreesToRadians
 from Classes.Hitbox import hitbox
 from ConstantsAndUtils import FieldMirroringUtils
 import subprocess
-
+from pygrabber.dshow_graph import FilterGraph
 
 
 team = "blue"
@@ -28,8 +28,18 @@ def grab_past_reef(reefSubscribers):
         for i, level in enumerate(reef):
             level.append(reefLevelBools[i])
             
-    return reef
+    return reef 
 
+def coralCameraIndex() -> int:
+    graph = FilterGraph()
+    
+    try:
+        device = graph.get_input_devices().index(Constants.CoralAndAlgaeCameraConstants.CORAL_CAMERA_NAME)
+    except ValueError:
+        device = 0
+        
+    return device
+    
 def main():
     def fetch_robot_position() -> tuple[Pose3d, float]:
         """
@@ -50,23 +60,25 @@ def main():
 
     # Reef Values
     reef = [[False for _ in range(4)] for _ in range(12)]
+    algae = [[False for _ in range(2)] for _ in range(12)]
     defaultReef = [False for _ in range(4)]
     defaultAlgae = [False for _ in range(2)]
+    algaeNotSeenCounterList = [[0 for _ in range(2)] for _ in range(12)]
 
     
     # Create an instance of the AprilTag camera
-    aprilTagCameraFront = AprilTagCamera(PhotonLibConstants.APRIL_TAG_FRONT_CAMERA_NAME, CameraConstants.ROBOT_TO_CAMERA_FRONT_TRANSFORMATION)
+    aprilTagCameraFront = AprilTagCamera(PhotonLibConstants.APRIL_TAG_FRONT_CAMERA_NAME, PhotonLibConstants.ROBOT_TO_CAMERA_FRONT_TRANSFORMATION)
+    aprilTagCameraBack = AprilTagCamera(PhotonLibConstants.APRIL_TAG_BACK_CAMERA_NAME, PhotonLibConstants.ROBOT_TO_CAMERA_BACK_TRANSFORMATION)
 
     # Grabs the Robot's topic and publisher
     robotPoseTopic = inst.getStructTopic("VisionRobotPose", Pose3d)
     robotPosePublisher = robotPoseTopic.publish()
-
     aprilTagCameraConnectionTopic = inst.getBooleanTopic("AprilTagCameraConnection")
     aprilTagCameraConnectionPublisher = aprilTagCameraConnectionTopic.publish()
     robotPosition = None
 
     # Reef Publishers and Subscribers
-    reefTable = inst.getTable("CoralLocations")
+    reefTable = inst.getTable("ReefLocationTable")
     reefL1Topic = reefTable.getBooleanArrayTopic("ReefL1")
     reefL2Topic = reefTable.getBooleanArrayTopic("ReefL2")
     reefL3Topic = reefTable.getBooleanArrayTopic("ReefL3")
@@ -85,18 +97,13 @@ def main():
     reefPose3dTable = inst.getTable("reefPose3dTable")
     pose3dTableTopic = reefPose3dTable.getStructArrayTopic("pose", Pose3d)
     pose3dPublisher = pose3dTableTopic.publish()
-    
 
-    
-
-    coralCamera = CoralCamera.CoralCamera()
+    coralCamera = CoralCamera.CoralCamera(cameraIndex=coralCameraIndex())
     coralCameraOpened = coralCamera.camera.isOpened()
     reefCameraConnectionPublisher.set(False)
     
     coralHitboxes = hitbox.makeCoralHitboxes()
     algaeHitboxes = hitbox.makeAlgaeHitboxes()
-
-
     
     while True:
         # Get April Tags from the camera
@@ -104,16 +111,14 @@ def main():
             inst.stopServer()
             cv2.destroyAllWindows()
             break
-
-        startTime = time.time()
         
-        if aprilTagCameraFront.isConnected():
+        if (aprilTagCameraFront.isConnected() or aprilTagCameraBack.isConnected()) and Constants.PhotonLibConstants.shouldTestAprilTags:
             aprilTagCameraConnectionPublisher.set(True)
             aprilTags = aprilTagCameraFront.get_tags()
             if aprilTags:
                 robotPosition, timestamp = fetch_robot_position()
                 if DriverStation.getAlliance == DriverStation.Alliance.kRed:
-                    robotPosition=robotPosition.relativeTo(FieldMirroringUtils.FIELD_WIDTH, FieldMirroringUtils.FIELD_HEIGHT, 0, Rotation3d)
+                    robotPosition = robotPosition.relativeTo(FieldMirroringUtils.FIELD_WIDTH, FieldMirroringUtils.FIELD_HEIGHT, 0, Rotation3d)
 
                 if robotPosition:
                     robotPosePublisher.set(robotPosition.estimatedPose, int(timestamp))
@@ -122,14 +127,14 @@ def main():
         if coralCamera.camera.isOpened() and robotPosition:
             reefCameraConnectionPublisher.set(True)
             reef = grab_past_reef(coralSubscribers)
-            coralCamera.camera_loop(reef, "algea", coralHitboxes, "none",5, robotPosition)
+            coralCamera.camera_loop(reef, algae, coralHitboxes, algaeHitboxes, algaeNotSeenCounterList, robotPosition)
             
             for level, publisher in enumerate(coralPublishers):
                 reefLevelBoolVals = []
                 for reefSection in reef:
                     reefLevelBoolVals.append(reefSection[level])
-                publisher.set(reefLevelBoolVals)
-                
+                publisher.set(reefLevelBoolVals) 
+                             
     time.sleep(0.01)
             
 if __name__ == "__main__":
